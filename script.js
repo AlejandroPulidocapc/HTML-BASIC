e217797f1065e96efdfcf53c89d3730479125af0
AlejandroPulidocapc <pulidocerinza@gmail.com>
2023-05-19 20:40:39 -0500

    HTML PRACTICANDO

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function mostrarDatos() {
+
+    fname = document.getElementById("fname").value
+    lname = document.getElementById("lname").value
+    document.getElementById("demo").innerHTML = fname + " " + lname;
+}
